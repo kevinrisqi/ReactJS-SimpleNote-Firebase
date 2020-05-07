8cd85b4b6249d88a7f2f157e048835247cf6458d
kevinrisqi22@gmail.com <kevinrisqi222>
2020-05-08 01:45:05 +0700

    Add configuration firebase

---
+++ src/config/firebase/index.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD0Y70uCKbG7yy5vLTu-3aAhoH5o-y5KgQ",
+    authDomain: "reactjs-simple-notes.firebaseapp.com",
+    databaseURL: "https://reactjs-simple-notes.firebaseio.com",
+    projectId: "reactjs-simple-notes",
+    storageBucket: "reactjs-simple-notes.appspot.com",
+    messagingSenderId: "32529683314",
+    appId: "1:32529683314:web:5d42a7ef50f8726c2343eb",
+    measurementId: "G-K0RG8HGX9Y"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
