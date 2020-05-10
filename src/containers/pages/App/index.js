48f941e937dca561ae7c1ef36da4f0b3e15db441
kevinrisqi22@gmail.com <kevinrisqi222>
2020-05-10 18:17:57 +0700

    Refactoring redux

--- src/containers/pages/App/index.js
+++ src/containers/pages/App/index.js
@@ -5,33 +5,8 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Dashboard from '../Dashboard';
 import Register from './../Register/index';
 import Login from './../Login/index';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-
-const initialState = {
-  popup: false,
-  isLogin: false
-}
-
-const reducer = (state = initialState, action) => {
-  if (action.type === 'CHANGE_POPUP') {
-    return {
-      ...state,
-      popup: action.value
-    }
-  }
-
-  if (action.type === 'CHANGE_ISLOGIN') {
-    return {
-      ...state,
-      isLogin: action.value
-    }
-  }
-  return state;
-}
-
-const store = createStore(reducer)
-
+import { store } from '../../../config/redux';
 
 function App() {
   return (
