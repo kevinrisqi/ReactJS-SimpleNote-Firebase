62e471f2fae6e0715f142a7bdd37ad38d3d28925
kevinrisqi22@gmail.com <kevinrisqi222>
2020-05-10 18:05:35 +0700

    Add redux

--- src/containers/pages/App/index.js
+++ src/containers/pages/App/index.js
@@ -5,24 +5,53 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Dashboard from '../Dashboard';
 import Register from './../Register/index';
 import Login from './../Login/index';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const initialState = {
+  popup: false,
+  isLogin: false
+}
+
+const reducer = (state = initialState, action) => {
+  if (action.type === 'CHANGE_POPUP') {
+    return {
+      ...state,
+      popup: action.value
+    }
+  }
+
+  if (action.type === 'CHANGE_ISLOGIN') {
+    return {
+      ...state,
+      isLogin: action.value
+    }
+  }
+  return state;
+}
+
+const store = createStore(reducer)
+
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/">
-            <Dashboard />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/">
+              <Dashboard />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
